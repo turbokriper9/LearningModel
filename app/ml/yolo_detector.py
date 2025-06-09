959a113ff8f44666c029383ed0b5eba957950f2f
Danil Mannanov <vildanmannanov2@gmail.com>
2025-06-09 14:49:24 +0500

    Исправил ошибки в файлах, добавл новую модель

--- app/ml/yolo_detector.py
+++ app/ml/yolo_detector.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 
 #модель YOLOv8
-model = YOLO("runs/detect/train3/weights/best.pt")
+model = YOLO("runs/detect/train4/weights/yolov11.pt")
 
 def detect_people_from_camera():
     cap = cv2.VideoCapture(0)
@@ -22,3 +22,20 @@ def detect_people_from_camera():
         boxes.append({"xmin": x1, "ymin": y1, "xmax": x2, "ymax": y2})
 
     return {"count": count, "boxes": boxes}
+
+def detect_people_from_image(img):
+    print("Классы модели:", model.names, flush=True)
+    cv2.imwrite("debug.jpg", img)  # Сохраняем изображение для отладки
+    results = model(img)
+    detections = results[0].boxes
+    count = 0
+    boxes = []
+    for box in detections:
+        if int(box.cls[0]) == 0:  # 0 — класс person
+            count += 1
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            boxes.append({"xmin": x1, "ymin": y1, "xmax": x2, "ymax": y2})
+    print("Обнаружено людей:", count)
+    return {"count": count, "boxes": boxes}
+
+print("Shape:", img.shape if img is not None else "None")
